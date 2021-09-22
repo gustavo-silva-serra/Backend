2d9a037f9c14034f481ba122fda8ca03d04d2033
gustavo-silva-serra <gustavo.silva.serra@gmail.com>
2021-09-22 00:26:57 -0300

    Refatoracao de conceito de produto e carrinho, implementacao de cache de descontos

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 #TODO: deixar registro das consultas e dos erros?
-#TODO: fazer a cache com tempo configurável e com tamanho limite
 #TODO: tentar reconectar de quanto em quanto tempo caso o serviço tenha ficado offline?
-#TODO: thread para atender clientes?
 #TODO: o que posso fazer em termos de validacao de input?
 #TODO: posso trocar facilmente o metodo de consulta do meu servico para banco de dados, por ex? strategy pattern
 #TODO: timeout para processar a consulta, como implementar?
@@ -10,12 +8,10 @@
 #TODO: tem como processar em lote?
 #TODO: adicionar o brinde com regras variaveis (strategy)
 #TODO: criar class abstrata para buscar produtos
-#TODO: encapsular produtos do banco em um objeto
 #OTOD: parametros do meu servico: porta http, tempo para cache, tamanho da fila
 #TODO: atender depois o REST
 #TODO: tratar exceoes de maneira mais graciosa
 #TOOD: salvar descontos em relatorios?
-#TODO: calcular totais com base nos produtos
 
 #TODO: posso trocar facilmente autenticação/protocolo da minha interface HTTP?
 #TODO: posso trocar o insecure_channel?
@@ -24,48 +20,78 @@ import discount_pb2
 import grpc
 import json
 import cherrypy
+import time
 
 class ProductDatabase:
     def __init__(self):
         products_db = json.loads(open('products.json').read())
         self.products_map = {product['id']:product for product in products_db}
     
-    def get_product_price(self,product_id):
+    def get_price(self,product_id):
         return int(self.products_map[product_id]['amount'])
 
 class DiscountEngine:
-    def get_product_discount(self,product_id):
+    cache_timeout = 0
+    cached = {}    
+    
+    def __init__(self, cache_t):
+        self.cache_timeout = cache_t 
+
+    def get_discount(self,product_id):
+        # Se existe uma cache, procura primeiro nela
+        if self.cache_timeout > 0:
+            if product_id in self.cached.keys():
+                if time.time() - self.cached[product_id][0] < self.cache_timeout:
+                    return self.cached[product_id][1]
+        
+        # Nao existe cache ou expirou    
         channel = grpc.insecure_channel('192.168.0.12:50051')
         stub = discount_pb2_grpc.DiscountStub(channel)
-        return float(stub.GetDiscount(discount_pb2.GetDiscountRequest(productID = product_id)).percentage)
+        perc = float(stub.GetDiscount(discount_pb2.GetDiscountRequest(productID = product_id)).percentage)
+        
+        # Atualiza o desconto na cache
+        if self.cache_timeout > 0:
+            if len(self.cached) > 10000: # Limite facilmente parametrizavel
+                self.cached = {}
+            self.cached[product_id] = (time.time(), perc)
+            
+        return perc
+
+class Product:
+    def __init__(self, prod_id, quantity, amount, discount_percentage):
+        self.id = int(prod_id)
+        self.quantity = int(quantity)
+        self.unit_amount = int(amount)
+        self.total_amount = self.quantity * self.unit_amount
+        self.discount = int(float(discount_percentage) * self.total_amount)
+        self.is_gift = False
+
+class Cart:
+    def __init__(self):
+        self.total_amount = 0
+        self.total_discount = 0
+        self.total_amount_with_discount = 0
+        self.products = []
+    
+    def add_product(self, product):
+        self.total_amount += product.total_amount
+        self.total_discount += product.discount
+        self.total_amount_with_discount = self.total_amount - self.total_discount
+        self.products.append(product)
 
 class ShopCart:
     
     def process(self, input_json):
-        total_amount = 0
-        total_discount = 0
-        
         input_json = json.loads(input_json)
-        products = []
+        cart = Cart()
         
         for product in input_json['products']:
-            amount = ProductDatabase().get_product_price(product['id'])
-            total_amount += amount * int(product['quantity'])
-            discount_percentage = DiscountEngine().get_product_discount(product['id'])
-            discount = discount_percentage * amount
-            total_discount += discount
-            products.append(self.create_default_product_response(product['id'], int(product['quantity']), amount, discount))
+            prod_id,prod_qnt = product['id'],product['quantity']
+            amount = ProductDatabase().get_price(prod_id)
+            discount_percentage = DiscountEngine(5).get_discount(prod_id)
+            cart.add_product(Product(prod_id, prod_qnt, amount, discount_percentage))
             
-            
-        return self.create_default_response(int(total_amount),int(total_discount),products) 
-    
-    def create_default_response(self, total_amount, total_discount, products):
-        response = {"total_amount": total_amount, "total_amount_with_discount": total_amount - total_discount, "total_discount": total_discount, "products": products}
-        return json.dumps(response,indent=3)
-
-    def create_default_product_response(self, id, qnt, unit, discount, gift = False):
-        return {"id": int(id), "quantity": qnt, "unit_amount": unit, "total_amount": qnt*unit, "discount": discount, "is_gift": gift}
-        
+        return json.dumps(cart, default=lambda o: o.__dict__)
 
 class ShopCartServer(object):
 
